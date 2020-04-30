09f4a5baec3f50a458f0038826137b90ceca2d31
Kamil Cichocki <61561747+kamilcichocki0@users.noreply.github.com>
2020-04-30 22:02:59 +0200

    .

--- zajęcia7/scripts/add_user.php
+++ zajęcia7/scripts/add_user.php
@@ -1,7 +1,20 @@
 <?php 
   session_start();
   if (!empty($_POST['name']) && !empty($_POST['surname']) && !empty($_POST['birthday']) && !empty($_POST['city'])) {
-    echo 'ok';
+    //echo $_POST['city'],"<br>";
+    //echo $_POST['name'],"<br>";
+    //echo $_POST['surname'],"<br>";
+    //echo $_POST['birthday'],"<br>";
+    $city = $_POST['city'];
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $birthday = $_POST['birthday'];
+
+    require_once './connect.php';
+    $sql = "INSERT INTO `user` (`id`, `cityid`, `name`, `surname`, `birthday`) VALUES (NULL, '$city', '$name', '$surname', '$birthday')";
+    mysqli_query($conn, $sql);
+
+    header('location: ../');
   }else{
     $_SESSION['error'] = 'Wypełnij wszystkie dane';
     header('location: ../?add_user=');
